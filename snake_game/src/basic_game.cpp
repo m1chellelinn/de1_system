e1d3981f84b6a684289e2eb212174714e72659b6
Michael Lin (WSL) <michaellin0902@gmail.com>
2025-06-01 19:28:46 -0700

    compile fix for basic snake game

--- snake_game/src/basic_game.cpp
+++ snake_game/src/basic_game.cpp
@@ -9,7 +9,23 @@
 
 #include "address_map_arm.hpp"
 #include "peripherals.hpp"
-#include <snake.hpp>
+
+#define SNAKE_GAME_BASE     0x00000100
+
+#define CMD_START_GAME 0
+#define CMD_END_GAME 1
+#define CMD_SNAKE_ADD 2
+#define CMD_SNAKE_DEL 3
+#define CMD_NEW_SCORE 4
+
+#define MSG_X_OFFSET 1
+#define MSG_Y_OFFSET 10
+#define MSG_CMD_OFFSET 24
+
+#define KEYCODE_UP 103
+#define KEYCODE_DOWN 108
+#define KEYCODE_LEFT 105
+#define KEYCODE_RIGHT 106
 
 
 static const char *const action_mappings[3] = {
